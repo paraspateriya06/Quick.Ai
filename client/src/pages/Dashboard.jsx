46df4c7375b8cd8b594a31e2e9b41cb8f526801a
Paras Mani Paterya <158201078+paraspateriya06@users.noreply.github.com>
2025-08-01 16:26:27 +0530

    SideBar Commit second

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { dummyCreationData } from '../assets/assets';
 
 const Dashboard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [creations, setCreations] = useState([]);
 
-export default Dashboard
+  const getDashboardData = async () => {
+    setCreations(dummyCreationData);
+  };
+
+  useEffect(() => {
+    getDashboardData();
+  }, []);
+
+  return (
+    <div className='h-full overflow-y-scroll p-6'>
+      {/* Render content here if needed */}
+    </div>
+  );
+};
+
+export default Dashboard;
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,33 +1,57 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { assets } from '../assets/assets';
+import { Menu, X } from 'lucide-react';
+import Sidebar from '../components/Sidebar';
+import { SignIn,useUser } from '@clerk/clerk-react';
 
 const Layout = () => {
-  return (
-    <div>
-      <Navbar/>
-        <Outlet/>
-      
+  const navigate = useNavigate();
+  const [sidebar, setSidebar] = useState(false);
+  const {user} = useUser()
+
+  return user ? (
+    <div className="flex flex-col items-start justify-start h-screen">
+      <nav className="w-full px-8 min-h-14 flex items-center justify-between border-b border-gray-200">
+        <img
+          src={assets.logo}
+          alt="QuickAi Logo"
+          className="w-32 sm:w-44 cursor-pointer"
+          onClick={() => navigate('/')}
+        />
+
+        {/* Toggle Sidebar Icon (Only on small screens) */}
+        {sidebar ? (
+          <X
+            className="w-6 h-6 text-gray-600 sm:hidden cursor-pointer"
+            onClick={() => setSidebar(false)}
+          />
+        ) : (
+          <Menu
+            className="w-6 h-6 text-gray-600 sm:hidden cursor-pointer"
+            onClick={() => setSidebar(true)}
+          />
+        )}
+      </nav>
+
+      <div className='flex-1 w-full  flex h-[calc(100vh-64px)]'>
+
+        <Sidebar sidebar={sidebar} setSidebar={setSidebar}/>
+        <div className='flex-1 bg-[#F4F7FB]'>
+      <Outlet />
+        </div>
+
+      </div>
+
+
+    </div>
+  ) :  (
+    <div className='flex items-center justify-center h-screen'>
+      <SignIn/>
+
+
     </div>
   )
-}
+};
 
-export default Layout
-
-
-// import { Outlet } from 'react-router-dom'
-// import Navbar from '../components/Navbar'
-// import Sidebar from '../components/Sidebar'
-
-// const Layout = () => {
-//   return (
-//     <div className='flex'>
-//       <Sidebar />
-//       <div className='ml-[150px] w-full'>
-//         <Navbar />
-//         <Outlet /> {/* <-- this is crucial */}
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Layout
+export default Layout;
